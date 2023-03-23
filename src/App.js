91eebe8891c222b7f29b090d442402f41f080b51
gerorodriguez <gero0808yt@gmail.com>
2023-03-23 17:44:32 -0300

    tailwind commit

--- src/App.js
+++ src/App.js
@@ -9,30 +9,29 @@ const App = () => {
         <button>Laguna</button>
         <button>Tour</button>
       </div>
-      <div className="Info">
-        <h1>Bienvenido a San gregorio</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam
-          eleifend mi in nulla.
-        </p>
-        <button className="bt1">Leer mas</button>
+      <div className="info">
+        <h2>Bienvenido a San gregorio</h2>
+        <div className="text">
+          <p>Pueblo ubicado al Sur de la provincia de Santa Fe.</p>
+          <button className="bt1">Leer mas</button>
+        </div>
       </div>
       <div className="picture">
-        <img src="hero.png" alt="SG" width="50%" />
+        <img src="hero.png" alt="SG" />
       </div>
-      <div className="Laguna">
+      <div className="laguna">
         <h2>La picasa</h2>
-        <div className="picture2">
-          <img src="pcs.png" width="20%" alt="picasa" />
-          <p>
+        <div className="pic">
+          <img src="pcs.png" className="h-72" alt="picas" />
+          <p className="inf3">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam
             eleifend mi in nulla
+            <div className="boton">
+              <button className="bt2">Leer mas</button>
+            </div>
           </p>
-          {/* fijarse el <p></p> */}
         </div>
-        <button className="bt2">Leer mas</button>
       </div>
     </div>
   );
