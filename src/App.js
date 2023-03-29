2ec4ee7c8978b950d26584259b6adf7e7311ca86
Geronimo <gero0808yt@gmail.com>
2023-03-29 18:17:02 -0300

    Card component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 const App = () => {
   return (
     <div className="main">
-      <div className="head">
+      <header className="head">
         <button>Home</button>
         <button>About us</button>
         <button>Laguna</button>
         <button>Tour</button>
-      </div>
+      </header>
       <div className="info">
-        <h1 className="welcome w-20">Bienvenido a San gregorio!</h1>
+        <h1 className="welcome">Bienvenido a San gregorio!</h1>
         <div className="text">
           <p>Pueblo ubicado al Sur de la provincia de Santa Fe.</p>
           <button className="bt1">Leer mas</button>
         </div>
       </div>
-      <div className="picture h-100 my-20">
+      <div className="picture">
         <img src="hero.png" className="" alt="SG" />
       </div>
       <div className="laguna">
-        <h2>La picasa</h2>
         <div className="pic">
           <img src="pcs.png" alt="picas" />
-          <p className="inf3">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam
-            eleifend mi in nulla
-            <div className="boton">
-              <button className="bt2 h-80">Leer mas</button>
+          <p className="parr">
+            <div className="fe">
+              <h4>La picasa, laguna por excelencia</h4>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Aliquam eleifend mi in nulla
+              <button className="bt2">Leer mas</button>
             </div>
           </p>
         </div>
+      </div>
+      <div className="visit">
+        <h1>Lugares para visitar</h1>
       </div>
     </div>
   );
---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div class="main">
+      <div class="head">
+        <button>Home</button>
+        <button>About us</button>
+        <button>Laguna</button>
+        <button>Tour</button>
+      </div>
+      <div class="Info">
+        <h1>Bienvenido a San gregorio</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam
+          eleifend mi in nulla.
+        </p>
+        <button class="Discover">Leer mas</button>
+        <img src="sangregorio.png" alt="SG"></img>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
